72173c5073e6bbff4bbeda66c526155f76422b15
katsuma0728 <katsuma20220728@gmail.com>
2023-09-12 04:20:30 +0900

    [Add]

---
+++ src/javaWcp/Chapter05.java
@@ -0,0 +1,19 @@
+package javaWcp;
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		// [String]型の変数を宣言し、自分の名前を代入
+		String str = "林　克磨";
+		// 自分の名前を出力
+		System.out.println(str);
+		// [String]型の変数に、好きな言葉を再代入
+		str = "七転び八起き";
+		// 好きな言葉を出力
+		System.out.println(str);
+		
+		// [int]型の定数を宣言し、ラッキーナンバーを代入
+		final int luckyNumber = 7;
+		// ラッキーナンバーを出力
+		System.out.println(luckyNumber);
+	}
+}
