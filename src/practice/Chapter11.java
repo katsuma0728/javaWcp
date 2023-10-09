4dbc0682172f56bd39863230c6515270f82237d9
katsuma0728 <katsuma20220728@gmail.com>
2023-10-09 14:51:30 +0900

    [add]practice

--- src/practice/Chapter11.java
+++ src/practice/Chapter11.java
@@ -2,14 +2,17 @@ package practice;
 
 public class Chapter11 {
 	public static void main(String[] args) {
-		int date = 20211010;
-		String content = "牛乳を買う";
-		
+		// メンバー変数
+		// 日付
+		int date = 20211010; // インスタンス変数
+		// タスク内容
+		String content = "牛乳を買う"; // インスタンス変数
+
 		Task task = new Task(date, content);
 		task.print();
-		
+
 		task.done();
-		
+
 		task.print();
 	}
 }
--- src/practice/Task.java
+++ src/practice/Task.java
@@ -1,23 +1,32 @@
 package practice;
 
 public class Task {
-	private int date;
-	
-	private String content;
-	
-	private boolean done = false;
-	
+	// メンバー変数
+	// 日付
+	private int date;// インスタンス変数
+
+	// タスク内容
+	private String content;// インスタンス変数
+
+	// 状態（trueで完了）
+	private boolean done = false;// インスタンス変数
+
+	// コンストラクタを作成
 	public Task(int date, String content) {
 		this.date = date;
 		this.content = content;
 	}
-	
+
+	// doneメゾッド
 	public void done() {
 		System.out.println("タスク「" + content + "]を完了します。");
+		// 状態を完了する
 		done = true;
 	}
-	
+
+	// printメゾッド
 	public void print() {
+		// タスク内容を表示する
 		if (done) {
 			System.out.println(date + "のタスク「" + content + "」は完了しています。");
 		} else {
