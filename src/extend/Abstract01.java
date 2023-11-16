48ffaa8bdc7a011828bbe1f7acb49fab5945fb01
katsuma0728 <katsuma20220728@gmail.com>
2023-11-16 22:56:19 +0900

    [add]Comment

--- src/extend/Abstract01.java
+++ src/extend/Abstract01.java
@@ -5,9 +5,15 @@ import extend.animal.Dog;
 public class Abstract01 {
 	public static void main(String[] args) {
 
+		// Dogクラスのインスタンスを生成
 		Dog dog = new Dog();
+		// Animalクラスのメゾッドを呼び出し
 		dog.eat();
+		// 抽象メゾットを実装したメゾッドを呼び出し
 		dog.cry();
+
+		// 抽象クラスはインスタンス生成するとコンパイルエラー
+		// Animal animal = new Animal();
 	}
 
 }
--- src/extend/animal/Animal.java
+++ src/extend/animal/Animal.java
@@ -1,11 +1,14 @@
 package extend.animal;
 
+// abstract修飾子を設定して、抽象クラスとして宣言
 public abstract class Animal {
 	protected String name;
 
+	// 共通メゾッドを記述
 	public void eat() {
 		System.out.println(name + "が食事をしました！");
 	}
 
+	// サブクラスごとに違う処理は、抽象メゾットにしてサブクラスに実装される
 	public abstract void cry();
 }
