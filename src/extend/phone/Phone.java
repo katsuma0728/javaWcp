606e5b8d135b04cb3aaf337e57e6b6ce5c9f65e5
katsuma0728 <katsuma20220728@gmail.com>
2023-12-11 20:59:20 +0900

    [add]Interface02

--- src/extend/phone/Phone.java
+++ src/extend/phone/Phone.java
@@ -3,8 +3,13 @@ package extend.phone;
 //Phoneインターフェイスの宣言
 public interface Phone {
 //	フィールドの宣言
-	int MAX_NUMBER_DIGITS = 11;
+	final int MAX_NUMBER_DIGITS = 11;
 
 //	public abstractが自動で追加される（抽象メゾッド）
 	void call(String number);
+
+	// defaultメゾッドの宣言
+	default void powerOff() {
+		System.out.println("電源を切ります。");
+	}
 }
