1edcc389411c7fe7da94931b7eb316f889a7bbf5
katsuma0728 <katsuma20220728@gmail.com>
2024-01-04 20:43:36 +0900

    [add]review_Chapter4,5

---
+++ src/javaWcp/Chapter05review.java
@@ -0,0 +1,15 @@
+package javaWcp;
+
+public class Chapter05review {
+	public static void main(String[] args) {
+		String name = "KATSUMA";
+		System.out.println(name);
+
+		name = "初志貫徹";
+		System.out.println(name);
+
+		final int luckyNumber = 7;
+		System.out.println(luckyNumber);
+
+	}
+}
---
+++ src/javaWcp/Variable01.java
@@ -0,0 +1,30 @@
+package javaWcp;
+
+public class Variable01 {
+
+	public static void main(String[] args) {
+		// int型の変数num1を宣言
+		int num1;
+		// String型の変数str1を宣言
+		String str1;
+		// num1を初期化
+		num1 = 10;
+		// str1を初期化
+		str1 = "Hello World!";
+		// num1の内容を出力
+		System.out.println(num1);
+		// str1の内容を出力
+		System.out.println(str1);
+
+		// num1に20を再代入
+		num1 = 20;
+		// num1の内容を出力
+		System.out.println(num1);
+
+		// int型の変数num2を宣言と同時に初期化
+		int num2 = 30;
+		// num2の内容を出力
+		System.out.println(num2);
+	}
+
+}
